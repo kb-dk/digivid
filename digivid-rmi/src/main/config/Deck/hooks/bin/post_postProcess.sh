9a2dbdb0f30028ddee203c777b4840f1d91b17a3
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-08-22 15:08:33 +0200

    Better printing of log statements

--- digivid-rmi/src/main/config/Deck/hooks/bin/post_postProcess.sh
+++ digivid-rmi/src/main/config/Deck/hooks/bin/post_postProcess.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
 echo "$PWD" >> ~/post_postProcess.log
-echo "$@" >>  ~/post_postProcess.log
-echo ""  >>  ~/post_postProcess.log
+
+while [ ! -z "$1" ]; do
+        echo -n "$1 " >>  ~/post_postProcess.log
+        shift
+done
+
+
+echo -e "\n\n"  >>  ~/post_postProcess.log
